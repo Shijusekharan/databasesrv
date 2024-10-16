b1ba61a284f394cc043abd1d718eb003e3024fe5
Shijusekharan <shijusekharan@gmail.com>
2024-10-16 20:08:46 +0100

    Create createtables.sql

---
+++ createtables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    HireDate DATE,
+    Salary DECIMAL(10, 2),
+    DepartmentID INT,
+    FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID)
+);
