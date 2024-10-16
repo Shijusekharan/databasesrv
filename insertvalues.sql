1760378f7914fafd75d22e5d9be4b64444f48834
Shijusekharan <shijusekharan@gmail.com>
2024-10-16 20:20:14 +0100

    Update insertvalues.sql

--- insertvalues.sql
+++ insertvalues.sql
@@ -1,2 +1,2 @@
-INSERT INTO Employees (EmployeeID, FirstName, LastName, Email, HireDate, Salary, DepartmentID)
-VALUES (1, 'John', 'Doe', 'john.doe@example.com', '2023-01-15', 55000.00, 2);
+INSERT INTO Employees (EmployeeID, FirstName, LastName, Email, HireDate, Salary)
+VALUES (1, 'John', 'Doe', 'john.doe@example.com', '2023-01-15', 55000.00);
